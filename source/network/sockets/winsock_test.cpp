f68c2c4c261c2501490996c9936c0261ec0120bd
杜修杏 <duxiuxing@foxmail.com>
2018-09-10 20:27:38 +0800

    SocketsTest工程上库

---
+++ source/network/sockets/winsock_test.cpp
@@ -0,0 +1,31 @@
+﻿#include "stdafx.h"
+#include "gtest/gtest.h"
+
+#include <WS2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+/*
+    TestedMethod: 
+    Description: 
+    Output:
+*/
+TEST(WinSockTest, IpAddressStringToNumber) {
+	TCHAR ip_string[] = _T("192.168.0.1");
+
+	//uint32_t ip_number1 = inet_addr(ip_string);
+
+	in_addr addr;
+	uint32_t ip_number = InetPton(AF_INET, ip_string, &addr);
+}
+
+/*
+TestedMethod:
+Description:
+Output:
+*/
+TEST(WinSockTest, IpAddressNumberToString) {
+	TCHAR ip_string[INET_ADDRSTRLEN] = {0};
+	in_addr addr;
+	addr.s_addr = 3245398923;
+	LPCTSTR ret = InetNtop(AF_INET, &addr, ip_string, INET_ADDRSTRLEN);
+}
