9c6540057e35e062780b1e5c694c53131768e4e8
duxiuxing <duxiuxing@foxmail.com>
2018-10-24 20:29:44 +0800

    示例代码上库：d.Determining the Parameters Supported by an Encoder

--- source/test/gdiplus/encoder_clsid.cpp
+++ source/test/gdiplus/encoder_clsid.cpp
@@ -8,11 +8,11 @@ using namespace Gdiplus;
 
 TEST(ImageCoders, GetEncoderClsid) {
     CLSID encoderClsid = {0};
-    INT result = Helper::GetEncoderClsid(L"image/png", &encoderClsid);;
+    INT result = Helper::GetEncoderClsid(L"image/png", &encoderClsid);
 
     ASSERT_TRUE(result != -1) << "The PNG encoder is not installed.";
 
-    WCHAR guid[39];
+	WCHAR guid[39] = {0};
     StringFromGUID2(encoderClsid, guid, 39);
     EXPECT_STREQ(L"{557CF406-1A04-11D3-9A73-0000F81EF32E}", guid);
 }
