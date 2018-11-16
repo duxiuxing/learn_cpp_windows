0ce46f72c1e50939e93a77be8a062b76947b0882
duxiuxing <duxiuxing@foxmail.com>
2018-11-16 14:47:25 +0800

    gdi_test工程上库

---
+++ source/test/gdi/mfc_dll_app.cpp
@@ -0,0 +1,21 @@
+﻿#include "stdafx.h"
+#include "mfc_dll_app.h"
+
+#ifdef _DEBUG
+    #define new DEBUG_NEW
+#endif
+
+BEGIN_MESSAGE_MAP(CMfcDllApp, CWinApp)
+END_MESSAGE_MAP()
+
+CMfcDllApp theApp;
+
+CMfcDllApp::CMfcDllApp()
+{}
+
+BOOL CMfcDllApp::InitInstance()
+{
+    CWinApp::InitInstance();
+
+    return TRUE;
+}
